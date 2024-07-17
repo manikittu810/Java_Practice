aa2980c1417859ccd6efff41c37f1643187d89f3
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-07-17 13:42:03 -0700

    Static Nested class

--- Oops/NestedClass/StaticNestedClass/OuterClass.java
+++ Oops/NestedClass/StaticNestedClass/OuterClass.java
@@ -1,13 +1,21 @@
-package InterviewPrep.Oops.NestedClass;
-
+package InterviewPrep.Oops.NestedClass.StaticNestedClass;
 public class OuterClass {
     int instanceVariable = 20;
     static int classStaticVariable = 10;
-    static class NestedClass{
+    static class NestedClass{ //This is a static nested class
         public void print(){
 //            System.out.println(instanceVariable + classStaticVariable);
             System.out.println(classStaticVariable);
         }
+    }
+    private static class NestedClass1{ // Nested class with private access specifier
+        public void print(){
+            System.out.println("This is a private static class");
+        }
+    }
+    public void display(){
+    NestedClass1 obj = new NestedClass1();
+        obj.print();
     }
 }
 class Main{
@@ -15,6 +23,8 @@ class Main{
     {
         OuterClass.NestedClass obj = new OuterClass.NestedClass();
         obj.print();
+        OuterClass obj1 = new OuterClass();
+        obj1.display();
     }
 }
 //in static Nested class you can only use static variables from the outer class not the instance variables
