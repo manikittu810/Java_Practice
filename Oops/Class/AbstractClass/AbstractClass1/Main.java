e3fb7b571e993b7a0e6332d73d6e8e5b45371f96
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-07-17 13:17:04 -0700

    Moved to different package

--- Oops/Class/AbstractClass/AbstractClass1/Main.java
+++ Oops/Class/AbstractClass/AbstractClass1/Main.java
@@ -1,10 +1,11 @@
-package InterviewPrep.Oops.Class.AbstractClass;
+package InterviewPrep.Oops.Class.AbstractClass.AbstractClass1;
 
 public class Main {
     public static void main(String[] args)
     {
-        Audi objAudi = new Audi(25);
-
+        Object obj = new Audi(24);
+        Audi objAudi = (Audi) obj; // To access methods specific to Audi cast the object
+//        Audi objAudi = new Audi(25) ;
         objAudi.pressDualBrakeSystem();
         objAudi.pressClutch();
         objAudi.pressBrake();
