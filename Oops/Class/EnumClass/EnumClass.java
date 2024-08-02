7b3ff8671fbd4609ea7c3cdb417641e3c2518b99
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-08-02 06:32:08 -0700

    ENUM class impl

---
+++ Oops/Class/EnumClass/EnumClass.java
@@ -0,0 +1,22 @@
+package InterviewPrep.Oops.Class.EnumClass;
+
+public  enum EnumClass {
+    MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY
+}
+class Main{
+    public static void main(String[] args) {
+        //when using enum class we need to know how to use it with
+        //values(),ordinal(),valueOf(),name()
+        //usage of values() and ordinal()
+        for(EnumClass obj : EnumClass.values()){
+            System.out.println(obj.ordinal());
+        }
+
+        //usage of valueOf() and name()
+       EnumClass obj1 = EnumClass.valueOf("SUNDAY");
+        System.out.println(obj1.name());
+
+
+
+    }
+}
