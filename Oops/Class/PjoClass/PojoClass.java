5ec536b140cb56dbeb2893d20773c6efdcb45abd
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-08-02 06:31:50 -0700

    POJO class

---
+++ Oops/Class/PjoClass/PojoClass.java
@@ -0,0 +1,30 @@
+package InterviewPrep.Oops.Class.PjoClass;
+
+public class PojoClass {
+    private int rollNumber;
+    private String sName;
+    public PojoClass(){
+
+    }
+    public void setRollNumber(int rollNumber){
+        this.rollNumber=rollNumber;
+    }
+    public void setsName(String sName){
+        this.sName =sName;
+    }
+    public int getRollNumber(){
+        return rollNumber;
+    }
+    public String getsName(){
+        return sName;
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        PojoClass obj = new PojoClass();
+        obj.setsName("smk");
+        System.out.println(obj.getsName());
+        obj.setRollNumber(1);
+        System.out.println(obj.getRollNumber());
+    }
+}
