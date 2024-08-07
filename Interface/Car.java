c07017bd8a313850e33cf1a31fc946b883c7a412
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-08-07 12:44:10 -0700

    Interface basic code

---
+++ Interface/Car.java
@@ -0,0 +1,19 @@
+package InterviewPrep.Interface;
+
+
+interface Car {
+     void applyBreak();
+}
+class CarImpl implements Car{
+
+    @Override
+    public void applyBreak() {
+        System.out.println("Break applied");
+    }
+}
+class Test{
+    public static void main(String[] args) {
+        CarImpl obj = new CarImpl();
+        obj.applyBreak();
+    }
+}
