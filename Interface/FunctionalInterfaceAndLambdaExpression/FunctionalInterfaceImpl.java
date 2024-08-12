7d07f265e65ffe1daff1fe3d234d6f373051c90f
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-08-12 15:17:20 -0700

    Funtional Interface Impl

---
+++ Interface/FunctionalInterfaceAndLambdaExpression/FunctionalInterfaceImpl.java
@@ -0,0 +1,19 @@
+package InterviewPrep.Interface.FunctionalInterfaceAndLambdaExpression;
+
+@FunctionalInterface
+public interface FunctionalInterfaceImpl {
+    void display();
+}
+
+class Impl implements FunctionalInterfaceImpl{
+
+    public void display(){
+        System.out.println("Functional Interface Implemented");
+    }
+}
+class Test{
+    public static void main(String[] args) {
+        FunctionalInterfaceImpl obj = new Impl();
+        obj.display();
+    }
+}
