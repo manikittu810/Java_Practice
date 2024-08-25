85869fe5770ee757dafc38fa06970455fb332d9e
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-08-24 19:58:34 -0700

    IllegalArgumentException Exception Example

---
+++ ExceptionHandling/RunTimeException/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package InterviewPrep.ExceptionHandling.RunTimeException;
+
+public class IllegalArgumentException {
+    public static void main(String[] args) {
+        int n= Integer.parseInt("abc");
+    }
+}
