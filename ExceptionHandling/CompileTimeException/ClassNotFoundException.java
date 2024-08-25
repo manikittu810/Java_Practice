7112bae50c7772753edac6c8859a42377fd40a80
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-08-24 20:27:57 -0700

    ClassNotFoundException Exception Example using try and catch blocks

--- ExceptionHandling/CompileTimeException/ClassNotFoundException.java
+++ ExceptionHandling/CompileTimeException/ClassNotFoundException.java
@@ -12,18 +12,38 @@
 
 //Using try/catch
 
+//package InterviewPrep.ExceptionHandling.CompileTimeException;
+//public class ClassNotFoundException extends Throwable {
+//
+//    public static void main(String[] args) throws ClassNotFoundException{
+//        method1();
+//    }
+//    @SuppressWarnings("All")
+//    public static void method1() throws ClassNotFoundException {
+//        try{
+//            throw new ClassNotFoundException();
+//        } catch(ClassNotFoundException exceptionObject){
+//            exceptionObject.printStackTrace();
+//        }
+//    }
+//}
+
+//or use this below
+
 package InterviewPrep.ExceptionHandling.CompileTimeException;
 public class ClassNotFoundException extends Throwable {
 
     public static void main(String[] args) throws ClassNotFoundException{
-        method1();
+        try{
+            method1();
+        } catch (ClassNotFoundException e) {
+            System.out.println("This is the exception object message : "+ e.getMessage());
+
+            throw new RuntimeException(e);
+        }
     }
     @SuppressWarnings("All")
     public static void method1() throws ClassNotFoundException {
-        try{
             throw new ClassNotFoundException();
-        } catch(ClassNotFoundException exceptionObject){
-            exceptionObject.printStackTrace();
-        }
     }
 }
