4fa5e3d83f3765f4af1a1c59e3a0b7f83d472261
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-08-24 19:42:34 -0700

    IndexOutOfBounds Exception example file added

---
+++ ExceptionHandling/RunTimeException/IndexOutOfBoundsException.java
@@ -0,0 +1,7 @@
+class IndexOutOfBoundsException{
+public static void main(String[] args){
+
+int[] a = new int[2];
+System.out.println(a[3]);
+}
+}
--- ExceptionHandling/RunTimeException/OutOfMemoryError.java
+++ ExceptionHandling/RunTimeException/OutOfMemoryError.java
@@ -1,3 +1,5 @@
+package InterviewPrep.ExceptionHandling.RunTimeException;
+
 class OutOfMemoryError{
 public static void main(String[] args){
 String[] arr = new String[900000000*900000000*900000000];
