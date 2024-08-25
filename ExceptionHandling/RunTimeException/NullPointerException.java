56bb38d644c5b614cccbf245ab9647d3606c42be
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-08-24 19:56:47 -0700

    NullPointerException Exception Example

---
+++ ExceptionHandling/RunTimeException/NullPointerException.java
@@ -0,0 +1,8 @@
+package InterviewPrep.ExceptionHandling.RunTimeException;
+
+public class NullPointerException {
+    public static void main(String[] args) {
+        String s = null;
+        System.out.println(s.charAt(0));
+    }
+}
