a5c800fb54e6c6f62f2d16db9fdcb85c32559430
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-09-01 22:27:58 -0700

    Implemented priority Queue

--- JCF/CollectIonMethodsImpl.java
+++ JCF/CollectIonMethodsImpl.java
@@ -1,10 +1,9 @@
 package InterviewPrep.JCF;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class CollectIonMethodsImpl {
+    @SuppressWarnings("All")
     public static void main(String[] args) {
         List<Integer> l = new ArrayList<>();
         l.add(2);
@@ -13,15 +12,15 @@ public class CollectIonMethodsImpl {
 
 
         System.out.println("Size   :" + l.size());
-        System.out.println("contains : " +  l.contains(5) );
-        System.out.println("IsEmpty :" + l.isEmpty() );
+        System.out.println("contains : " + l.contains(5));
+        System.out.println("IsEmpty :" + l.isEmpty());
         l.add(5);
-        System.out.println("contains :" +  l.contains(5) );
+        System.out.println("contains :" + l.contains(5));
         l.remove(3);
-        System.out.println("Removed using Index  :" +  l.contains(5) );
+        System.out.println("Removed using Index  :" + l.contains(5));
         //remove using the object , remove the first occurrence of value
         l.remove(Integer.valueOf(3));
-        System.out.println("removed using object :" +  l.contains(3) );
+        System.out.println("removed using object :" + l.contains(3));
 
         Stack<Integer> st = new Stack<>();
         st.add(6);
@@ -36,17 +35,52 @@ public class CollectIonMethodsImpl {
         System.out.println("adAll() test using containsAll : " + l.containsAll(st));
 
         l.removeAll(st);
-        System.out.println("removeAll : "+l.contains(8));
+        System.out.println("removeAll : " + l.contains(8));
         l.clear();
         System.out.println("Cleared the list : " + l);
 
+        //Priority Queue Implementation minHeap
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(1);
+        pq.add(10);
+        pq.add(-1);
+        pq.add(-100);
+        pq.add(200);
+        System.out.println("Using Iterator to print values");
+        Iterator<Integer> iterator = pq.iterator();
+        while (iterator.hasNext()) {
+            int val = iterator.next();
+            System.out.println(val);
+        }
+        System.out.println("Printing values using forEach method");
+        pq.forEach((Integer x) -> System.out.println(x));
+
+        System.out.println("polling values from Priority Queue");
+        while (!pq.isEmpty()) {
+            int val1 = pq.poll();
+            System.out.print(val1 + " ");
+            if (pq.contains(-1)) {
+                pq.remove(Integer.valueOf(-1));
+            }
+            System.out.println();
+        }
+
+        //Priority Queue Implementation maxHeap
 
 
+        PriorityQueue<Integer> p = new PriorityQueue<>((Integer a, Integer b) -> b - a);
 
+        p.add(100);
+        p.add(200);
+        p.add(-100);
+        p.add(-400);
+        p.add(-500);
 
-
-
-
-
+        while(!p.isEmpty()){
+            int val3 = p.poll();
+            System.out.println("Max Heap Poll : " + val3);
+        }
     }
 }
