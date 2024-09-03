439b13c9a071fd7ac8d6e18b0d3e7293d78dbd15
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-09-03 11:34:16 -0700

    HashMap and LinkedHashMap Impl

--- HashMapImpl/HashMapImpl.java
+++ HashMapImpl/HashMapImpl.java
@@ -2,15 +2,16 @@ package InterviewPrep.HashMapImpl;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class HashMapImpl {
     public static void main(String[] args) {
         Map<Integer,String> map = new HashMap<>();
+        map.put(3,"Zero");
         map.put(null,"TEST");
         map.put(0,null);
         map.put(1,"A");
-        map.put(3,"Zero");
         map.put(2,"B");
 
         map.putIfAbsent(null,"test");
@@ -48,6 +49,38 @@ public class HashMapImpl {
             String s = entry.getValue();
             System.out.println("Key :" +k + " value : " + s);
         }
+        System.out.println();
 
+        map.forEach((Integer n,String s) -> System.out.println(n+"......"+s));
+
+        System.out.println();
+
+        Map<Integer,String> map2 = new LinkedHashMap<>(); //maintains insertion order
+        map2.put(null,"TEST");
+        map2.put(0,null);
+        map2.put(1,"A");
+        map2.put(3,"Zero");
+        map2.put(2,"B");
+        map2.put(null,"test");
+        map2.put(4,"s");
+        map2.put(5,"M");
+        map2.forEach((Integer n,String s ) -> System.out.println(n +"...."+s));
+        System.out.println();
+
+
+        Map<Integer,String> map1 = new LinkedHashMap<>(16,0.75f,true); // maintains access order
+        map1.put(null,"TEST");
+        map1.put(0,null);
+        map1.put(1,"A");
+        map1.put(3,"Zero");
+        map1.put(2,"B");
+        map1.put(3,"Zero");
+        map1.put(1,"A");
+        map1.put(0,null);
+        map1.put(null,"TEST");
+        map1.put(null,"test");
+        map1.put(4,"s");
+        map1.put(5,"M");
+        map1.forEach((Integer n,String s ) -> System.out.println(n +"...."+s));
     }
 }
