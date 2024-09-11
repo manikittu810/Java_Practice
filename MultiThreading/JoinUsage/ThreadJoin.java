c115826554a05f56888337815da01dfa31d357ec
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-09-10 18:56:54 -0700

    Using join(),setDaemon() method

--- MultiThreading/JoinUsage/ThreadJoin.java
+++ MultiThreading/JoinUsage/ThreadJoin.java
@@ -7,7 +7,17 @@ public class ThreadJoin {
             Thread.sleep(3000);
             System.out.println(Thread.currentThread().getName() + " lock released...");
         } catch (InterruptedException e) {
-            System.out.println(Thread.currentThread().getName() + " Interrupted during sleep..");
+            System.err.println(Thread.currentThread().getName() + " Interrupted during sleep..");
+            Thread.currentThread().interrupt();
+        }
+    }
+    public synchronized void receiveCall1() throws InterruptedException {
+        try {
+            System.out.println(Thread.currentThread().getName() + " lock acquired...");
+            Thread.sleep(4000);
+            System.out.println(Thread.currentThread().getName() + " lock released...");
+        } catch (InterruptedException e) {
+            System.err.println(Thread.currentThread().getName() + " Interrupted during sleep..");
             Thread.currentThread().interrupt();
         }
     }
@@ -23,15 +33,28 @@ public class ThreadJoin {
                     throw new RuntimeException(e);
                 }
             });
+            Thread t2 = new Thread(() -> {
+                try {
+                    obj.receiveCall1();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            t2.setDaemon(true);
             t1.start();
+            t2.start();
             try {
                 System.out.println(Thread.currentThread().getName() + " thread is waiting to Join with Thread t1.");
                 Thread.sleep(3000);
                 t1.join();
+                System.out.println(Thread.currentThread().getName() + " joined with t1");
+                System.out.println(Thread.currentThread().getName() + " thread is waiting to Join with Thread t2.");
+                t2.join();
+                System.out.println(Thread.currentThread().getName() + " joined with t2");
             } catch (InterruptedException e) {
                 System.out.println("Main thread is waiting to Join......");
                 Thread.currentThread().interrupt();
             }
-            System.out.println("Main Thread exiting after t1 joining it..... : " + Thread.currentThread().getName());
+            System.out.println("Main Thread exiting after t1 and t2 joining it..... : " + Thread.currentThread().getName());
         }
     }
