3fa45592ca0f94c2f16e186c4e4f78247b6dffbf
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-12-05 12:14:06 -0800

    SubsetsTwo Backtracking

--- Backtracking/SubsetsTwo.java
+++ Backtracking/SubsetsTwo.java
@@ -5,36 +5,35 @@ import java.util.List;
 import java.util.Scanner;
 
 public class SubsetsTwo {
-    public static List<List<Integer>> subsetsTwo(int[] a){
+    public  List<List<Integer>> subsetsTwo(int[] a) {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> ans = new ArrayList<>();
-        int start =0;
-        backTrack(res,ans,a,start);
-        // Set<List<Integer>> s = new HashSet<>(res);
-        // for(List<Integer> l : res){
-        //     s.add(l);
-        // }
-        // List<List<Integer>> res1 = new ArrayList<>(s);
-        //return res1;
+        int start = 0;
+        backTrack(res, ans, a, start);
         return res;
     }
-    public static void backTrack(
+
+    private  void backTrack(
             List<List<Integer>> res,
             List<Integer> ans,
             int[] a,
             int start
-    ){
+    ) {
         res.add(new ArrayList<>(ans));
-        for(int i=start;i<a.length;i++){
-            if(i>start && a[i]==a[i-1]){
+        for (int i = start; i < a.length; i++) {
+            if (i > start && a[i] == a[i - 1]) {
                 continue;
             }
             ans.add(a[i]);
-            backTrack(res,ans,a,i+1);
-            ans.remove(ans.size()-1);
+            backTrack(res, ans, a, i + 1);
+            ans.removeLast();
         }
     }
+}
+class TestSubsetTwo{
+
     public static void main(String[] args){
+        SubsetsTwo obj = new SubsetsTwo();
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         for(int i = 0;i<t;i++){
@@ -43,8 +42,8 @@ public class SubsetsTwo {
             for(int j=0;j<n;j++){
                 a[j]= sc.nextInt();
             }
-            List<List<Integer>> res = subsetsTwo(a);
-            System.out.println(res);
+            System.out.println(obj.subsetsTwo(a));
         }
+        sc.close();
     }
 }
