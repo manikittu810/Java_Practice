02115c08d5e2913051e0757ec88f016d7e58c668
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-12-29 21:57:34 -0800

    Count of a subset sum with a given sum - Tabulation

--- dynamicProgramming/knapSack/CountofSubSetSumWithAGivenSum.java
+++ dynamicProgramming/knapSack/CountofSubSetSumWithAGivenSum.java
@@ -1,4 +1,38 @@
 package InterviewPrep.dynamicProgramming.knapSack;
 
+import java.util.Scanner;
+
 public class CountofSubSetSumWithAGivenSum {
+    public int countSubSets(int[]a ,int sum){
+        int n = a.length;
+        int[][]dp = new int[n+1][sum+1];
+        dp[0][0] = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(j>=a[i-1]){
+                    dp[i][j] = dp[i-1][j-a[i-1]]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+}
+class TestFunction{
+    public static void main(String[] args) {
+        CountofSubSetSumWithAGivenSum obj = new CountofSubSetSumWithAGivenSum();
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++){
+            int n = sc.nextInt();
+            int[]a = new int[n];
+            for(int j=0;j<n;j++){
+                a[j]= sc.nextInt();
+            }
+            int W = sc.nextInt();
+            System.out.println(obj.countSubSets(a,W));
+        }
+    }
 }
