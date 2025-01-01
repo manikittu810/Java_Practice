73af106f99e1aec64d6e8d813544fb33ed8b7855
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-12-31 20:27:15 -0800

    Count the Number of subsets with a given difference - space optimized

--- dynamicProgramming/knapSack/CountTheNumberOfSubsetsWithGivenDifference.java
+++ dynamicProgramming/knapSack/CountTheNumberOfSubsetsWithGivenDifference.java
@@ -9,20 +9,32 @@ public class CountTheNumberOfSubsetsWithGivenDifference {
         for(int i : a){
             sum+=i;
         }
+        if((sum+diff)%2!=0){
+            return 0;
+        }
         int sum1 = (diff+sum)/2;
-        int[][]dp = new int[n+1][sum1+1];
-        dp[0][0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=sum1;j++){
-                if(j>=a[i-1]){
-                    dp[i][j] = dp[i-1][j-a[i-1]]+dp[i-1][j];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
+//        int[][]dp = new int[n+1][sum1+1];
+//        dp[0][0]=1;
+//        for(int i=1;i<=n;i++){
+//            for(int j=0;j<=sum1;j++){
+//                if(j>=a[i-1]){
+//                    dp[i][j] = dp[i-1][j-a[i-1]]+dp[i-1][j];
+//                }
+//                else{
+//                    dp[i][j] = dp[i-1][j];
+//                }
+//            }
+//        }
+//        return dp[n][sum1];
+        //the below is space optimized 1D dp sc : O(sum1+1)
+        int[]dp = new int[sum1+1];
+        dp[0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=sum1;j>=a[i];j--){
+                dp[j]+=dp[j-a[i]];
             }
         }
-        return dp[n][sum1];
+        return dp[sum1];
     }
 }
 class TestGivenDifference{
@@ -43,3 +55,8 @@ class TestGivenDifference{
         sc.close();
     }
 }
+
+//Input: arr[] =  [5, 2, 6, 4], d = 3
+//Output: 1
+//Explanation: There is only one possible partition of this array. Partition : {6, 4}, {5, 2}.
+// The subset difference between subset sum is: (6 + 4) - (5 + 2) = 3.
